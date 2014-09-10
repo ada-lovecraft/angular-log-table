e44b5e8f71d781215304257caf7b8bb20f089cfe
Rai Butera <rai@rbutera.com>
2014-09-10 19:19:12 +0100

    renamed module to angular-log-table to adhere to angular best practices

--- ngLogTable.js
+++ ngLogTable.js
@@ -1,14 +1,16 @@
 'use strict';
 
-angular.module('ngLogTable', [])
+angular.module('angular-log-table', [])
   .config(function ($provide) {
       $provide.decorator('$log', function($delegate){
         $delegate.table = function() {
           var args = [].slice.call(arguments);
-          if(window.console && window.console.table)
+          if(window.console && window.console.table){
             console.table(args[0], args[1]);
-          else
-            $delegate.log(null, args)
+          }
+          else{
+            $delegate.log(null, args);
+          }
         };
         return $delegate;
       });
